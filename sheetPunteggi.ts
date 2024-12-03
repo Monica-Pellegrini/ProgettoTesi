6b0635d99829f7df251b2104cf3c73353f28c13b
Monica-Pellegrini <162020398+Monica-Pellegrini@users.noreply.github.com>
2024-12-03 18:40:32 +0100

    feat: Implement SheetPunteggi class
      Define SheetPunteggi class extending SpreadsheetManager.
      Add methods to initialize, get header, and retrieve scores data.

---
+++ sheetPunteggi.ts
@@ -0,0 +1,41 @@
+//class to manage the tabellaPunteggi
+class SheetPunteggi extends SpreadsheetManager
+{
+  private header: string[] | null;
+  private punteggiData: any[][];
+
+  constructor(sheetName: string)
+  {
+    super(sheetName);
+    this.header = null;
+    this.punteggiData = [];
+    this.initialize();
+  }
+
+  initialize(): void
+  {
+    let rawData = this.getData();
+
+    rawData.splice(0, 2); //remove the first two lines to create the tabPunteggi
+    this.header = rawData.shift() as string[]; //get the header line
+    this.punteggiData = rawData;
+  }
+
+  //method that returns the criteria contained in the TabellaPunteggi
+  getPunteggiData(): any[][]
+  {
+    return this.punteggiData;
+  }
+
+  //method that returns the header contained in the TabellaPunteggi
+  getHeader()
+  {
+    return this.header;
+  }
+}
+
+function prova()
+{
+    let p = new SheetPunteggi('ElencoCandidati');
+    p.getPunteggiData();
+}
