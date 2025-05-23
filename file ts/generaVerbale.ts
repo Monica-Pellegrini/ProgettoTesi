8ffa578ef9b884cd37d4a6d6068a905cbcbfcea7
FrancescoGirardello <162924304+FrancescoGirardello@users.noreply.github.com>
2025-05-23 13:41:12 +0200

    Delete file ts directory

--- file ts/generaVerbale.ts
+++
@@ -1,8 +0,0 @@
-function generaVerbale(): void {
-  try {
-    const verbale: VerbalGenerator = new VerbalGenerator();
-    verbale.generaVerbale();
-  } catch (e: unknown) {
-    SpreadsheetApp.getUi().alert("Errore", String(e), SpreadsheetApp.getUi().ButtonSet.OK);
-  }
-}
--- file ts/onOpen.ts
+++
@@ -1,5 +0,0 @@
-function onOpen(): void {
-  const menu = SpreadsheetApp.getUi().createMenu("Funzioni Aggiuntive");
-  menu.addItem("Genera Verbale", "generaVerbale");
-  menu.addToUi();
-}
